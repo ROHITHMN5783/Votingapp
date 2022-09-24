850dd4e87e1b10a937c0f0392e4e7a8c88f4de01
ROHITHMN5783 <rohithmn41@gmail.com>
2022-09-24 20:18:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      locations:[
+        {name:"bangalore",votes:0},
+        {name:"bellary",votes:0},
+        {name:"bellur",votes:0},
+        {name:"mysore",votes:0},
+
+      ],
+    };
+  }
+  vote(i){
+    let newlocations=[...this.state.locations];
+    newlocations[i].votes++;
+    this.setState({locations:newlocations})
+  }
+
+    render() {
+      return(
+        <>
+        <h1>DISPLAY THE CITIES TO BE VOTED</h1>
+        <div className="locations">
+        {this.state.locations.map((loc,i) =>(
+          <div key={i} className="location">
+            <div className="votecount" >{loc.votes}</div>
+            <div className="locationname">{loc.name}</div>
+            <button onClick={this.vote.bind(this ,i)}>Click </button>
+            </div>
+
+    ))}  
+   
+      </div>
+      </>
+      );
+        }
+      }
+        
+
+      
 export default App;
